8ac40b78d7eb15f56e9af3d6ea2823bd6f03f23d
Ian Moore <ianmooregis@gmail.com>
2017-08-26 21:13:26 -0400

    Updated todos location to include uid

--- app/app.jsx
+++ app/app.jsx
@@ -11,16 +11,13 @@ import router from 'app/router/';
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         store.dispatch(actions.login(user.uid));
+        store.dispatch(actions.startAddTodos());
         hashHistory.push('/todos');
     } else {
         store.dispatch(actions.logout());
         hashHistory.push('/');
     }
 });
-
-//import './../playground/firebase/index';
-
-store.dispatch(actions.startAddTodos());
 
 // Load foundation
 $(document).foundation();
